099f392bf97a7bfc5c1845f2cba70cbb2cd5a9e4
KanumuriHimasri1709 <himabindusep7@gmail.com>
2026-07-29 18:49:10 +0530

    Updated Numpy Day1 Arrays

--- Numpys/day_arrays.py
+++ Numpys/day_arrays.py
@@ -2,3 +2,10 @@ import numpy as np
 a=np.array([1,2,3])
 print(a)
 
+#2D Arrays
+b=np.array([[1,2,3],[4,5,6]])
+print(b)
+
+#3D Arrays
+c=np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(c)
