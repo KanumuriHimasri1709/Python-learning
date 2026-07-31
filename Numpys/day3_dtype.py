40b585568aa78af88d565c85ab1512d20990448c
KanumuriHimasri1709 <himabindusep7@gmail.com>
2026-07-31 18:32:59 +0530

    Updated the Numpys file

---
+++ Numpys/day3_dtype.py
@@ -0,0 +1,4 @@
+#To find the data type of an array, we can use the dtype attribute. The dtype attribute returns the data type of the array elements.
+import numpy as np
+arr = np.array([10,20,30,40,50,60])
+print(arr.dtype)
