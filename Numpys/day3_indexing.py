6528fd47aeca3628e3bc3783c4e971c0729d14a6
KanumuriHimasri1709 <himabindusep7@gmail.com>
2026-07-31 18:55:39 +0530

    Updated Numpys

---
+++ Numpys/day3_indexing.py
@@ -0,0 +1,7 @@
+#Indexing means in the array where the elements are stored. The first element is stored at index 0, the second element is stored at index 1, and so on. The last element is stored at index -1, the second last element is stored at index -2, and so on.
+import numpy as np
+arr = np.array([10,20,30,40,50])
+print(arr[0])
+print(arr[-1])
+print(arr[3])
+print(arr[-4])
