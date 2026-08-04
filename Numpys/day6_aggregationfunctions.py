e4833870b1f7409f373f92ca6f067b8a88542a36
KanumuriHimasri1709 <himabindusep7@gmail.com>
2026-08-04 20:11:23 +0530

    Updated Numpy files

--- Numpys/day6_aggregationfunctions.py
+++ Numpys/day6_aggregationfunctions.py
@@ -6,3 +6,11 @@ print(np.mean(arr))
 print(np.max(arr))
 print(np.min(arr))
 print(np.std(arr))
+
+
+#Another example 
+arr1 = np.array([2,4,6,8])
+print(np.sum(arr1))
+print(np.mean(arr1))
+print(np.max(arr1))
+print(np.min(arr1))
