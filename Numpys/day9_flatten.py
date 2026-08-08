fecd7451aabed0dfa693c251e0e658b49751dec4
KanumuriHimasri1709 <himabindusep7@gmail.com>
2026-08-08 20:42:19 +0530

    Updated Numpy files

---
+++ Numpys/day9_flatten.py
@@ -0,0 +1,4 @@
+#flatten is used to convert a multi-dimensional array into a one-dimensional array. It returns a copy of the original array collapsed into one dimension.
+import numpy as np
+arr = np.array([[10,20],[30,40],[50,60]])
+print(arr.flatten())
