66d4996d77165e4b4b9f1a4ab633b25c155ee629
KanumuriHimasri1709 <himabindusep7@gmail.com>
2026-08-09 11:34:59 +0530

    Updated the Numpy files

---
+++ Numpys/day10_transpose.py
@@ -0,0 +1,4 @@
+#Transpose of an array is used to convert either a rows into columns or columns into rows. It is denoted by A^T. The transpose of a matrix is obtained by interchanging the rows and columns of the matrix.
+import numpy as np
+arr = np.array([[1,2,3],[4,5,6]])
+print(arr.T)
