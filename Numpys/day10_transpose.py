fa64fcdc7affb061b8899469c1ac276b2dadc233
KanumuriHimasri1709 <himabindusep7@gmail.com>
2026-08-09 11:44:49 +0530

    Updated the numpy files

--- Numpys/day10_transpose.py
+++ Numpys/day10_transpose.py
@@ -2,3 +2,8 @@
 import numpy as np
 arr = np.array([[1,2,3],[4,5,6]])
 print(arr.T)
+
+#Another example of transpose of an array is given below
+arr1 = np.array([[10,20],[30,40],[50,60]])
+print(arr1.T)
+print(arr1.T.shape)
