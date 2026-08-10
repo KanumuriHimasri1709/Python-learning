534b5f974b0f7c91267b9757f70b3fba5392485d
KanumuriHimasri1709 <himabindusep7@gmail.com>
2026-08-10 18:47:47 +0530

    Updated the Numpy files

--- Numpys/day11_axis.py
+++ Numpys/day11_axis.py
@@ -5,3 +5,11 @@ print(np.sum(arr, axis=0))  # Sum along the rows (axis 0)
 print(np.sum(arr, axis=1))  # Sum along the columns (axis 1)
 print(np.max(arr, axis=0))  # Max along the rows (axis 0)
 print(np.max(arr, axis=1))  # Max along the columns (axis 1)
+
+
+#another example
+arr = np.array([[1,2],[3,4],[5,6]])
+print(np.sum(arr, axis=0))  # Sum along the rows (axis 0)
+print(np.sum(arr, axis=1))  # Sum along the columns (axis 1)
+print(np.min(arr, axis=0))  # Min along the rows (axis 0)
+print(np.min(arr, axis=1))  # Min along the columns (axis 1)
