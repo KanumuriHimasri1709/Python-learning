c9055012245107d1af42b09b2fc727e3ce3d2e00
KanumuriHimasri1709 <himabindusep7@gmail.com>
2026-08-10 18:45:10 +0530

    Updated the Numpy files

---
+++ Numpys/day11_axis.py
@@ -0,0 +1,7 @@
+#axis  is used to access the specific axis of the array. It is used in various numpy functions to specify the axis along which the operation should be performed. For example, in a 2D array, axis=0 refers to the rows and axis=1 refers to the columns.
+import numpy as np
+arr = np.array([[1,2],[3,4],[5,6]])
+print(np.sum(arr, axis=0))  # Sum along the rows (axis 0)
+print(np.sum(arr, axis=1))  # Sum along the columns (axis 1)
+print(np.max(arr, axis=0))  # Max along the rows (axis 0)
+print(np.max(arr, axis=1))  # Max along the columns (axis 1)
