186d5f1a4f878d0e6d27295f9643cbf876fb7468
KanumuriHimasri1709 <himabindusep7@gmail.com>
2026-08-13 21:41:45 +0530

    Updated Numpy files

---
+++ Numpys/day12_zeros.py
@@ -0,0 +1,4 @@
+# np.zeros it creates an array filled with zeros. The shape of the array is defined by the user. It can be used to initialize an array before populating it with actual data.
+import numpy as np
+arr = np.zeros(5)
+print(arr)
