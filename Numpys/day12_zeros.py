26af72c6e5b23179434497bd388450c309465acf
KanumuriHimasri1709 <himabindusep7@gmail.com>
2026-08-13 21:49:42 +0530

    Updated Numpy file

--- Numpys/day12_zeros.py
+++ Numpys/day12_zeros.py
@@ -2,3 +2,8 @@
 import numpy as np
 arr = np.zeros(5)
 print(arr)
+
+
+#Another example
+arr1 = np.zeros((2, 3),dtype=int)  # Create a 2x3 array filled with zeros and the data type is integer
+print(arr1)
