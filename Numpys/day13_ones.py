536dd0dd5459f17ad134e27cf0b9f562fbe78833
KanumuriHimasri1709 <himabindusep7@gmail.com>
2026-08-15 10:59:48 +0530

    Updated Numpy files

--- Numpys/day13_ones.py
+++ Numpys/day13_ones.py
@@ -7,3 +7,7 @@ print(arr)
 #another example of np.ones is to create a 2D array of shape (3, 4) filled with ones.
 arr2d = np.ones((3, 4))
 print(arr2d)
+
+#another example with integer data type
+arr_int = np.ones((2, 3), dtype=int)
+print(arr_int)
