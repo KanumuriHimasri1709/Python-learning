ade7ec5d0f640fbbd60dc6b26f2388500fee813d
KanumuriHimasri1709 <himabindusep7@gmail.com>
2026-08-22 18:26:34 +0530

    Updated Numpy files

---
+++ Numpys/day16_arange.py
@@ -0,0 +1,4 @@
+#np.arange is used to create an array with evenly spaced values within a given interval. It is similar to the built-in range function but returns an array instead of a list.
+import numpy as np
+arr = np.arange(2,8)
+print(arr)
