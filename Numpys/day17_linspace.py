0f80a72c141d772dcb0c1d5b5d062af85966f156
KanumuriHimasri1709 <himabindusep7@gmail.com>
2026-08-25 17:54:38 +0530

    Updated Numpy files

---
+++ Numpys/day17_linspace.py
@@ -0,0 +1,4 @@
+#np.linspace is used to create an array with evenly spaced values over a specified range. It is similar to np.arange but allows you to specify the number of values you want in the array.
+import numpy as np
+arr = np.linspace(2,10,5)
+print(arr)
