f97b3524aa901f1ef1419e6563b8b0b7bff87b9b
KanumuriHimasri1709 <himabindusep7@gmail.com>
2026-08-31 21:07:57 +0530

    Updated Numpy files

---
+++ Numpys/day18_randint.py
@@ -0,0 +1,4 @@
+#np.random.randint() - Generates random integers from a specified range. The function takes three parameters: the lower bound (inclusive), the upper bound (exclusive), and the size of the output array. In this example, it generates an array of 5 random integers between 1 and 9.
+import numpy as np
+arr = np.random.randint(1,10,5)
+print(arr)
