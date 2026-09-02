74b276fc5753ef43e8c7a449548135cf7616726d
KanumuriHimasri1709 <himabindusep7@gmail.com>
2026-09-02 18:39:18 +0530

    Update the Numpy files

--- Numpys/day19_random_choice.py
+++ Numpys/day19_random_choice.py
@@ -2,3 +2,7 @@
 import numpy as np
 arr = np.array(["Red", "Blue", "Green"])
 print(np.random.choice(arr)) #selects a random color from the array
+
+
+#another example of np.random.choice() is to select a random number from a given range
+print(np.random.choice(arr,2))
