c7b9ea4322bb6329b34a1298b0c2688a3a9a56c3
KanumuriHimasri1709 <himabindusep7@gmail.com>
2026-09-02 18:41:20 +0530

    Updated Numpy files

--- Numpys/day19_random_choice.py
+++ Numpys/day19_random_choice.py
@@ -6,3 +6,6 @@ print(np.random.choice(arr)) #selects a random color from the array
 
 #another example of np.random.choice() is to select a random number from a given range
 print(np.random.choice(arr,2))
+
+
+print(np.random.choice(10,2)) # selects 2 random numbers from the range of 0 to 9
