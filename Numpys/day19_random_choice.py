86f5cdd7ba087c664f1271f2872adfe9a37dabc5
KanumuriHimasri1709 <himabindusep7@gmail.com>
2026-09-02 18:35:54 +0530

    Updated Numpy files

---
+++ Numpys/day19_random_choice.py
@@ -0,0 +1,4 @@
+#np.random.choice.py() is used to select by your choice
+import numpy as np
+arr = np.array(["Red", "Blue", "Green"])
+print(np.random.choice(arr)) #selects a random color from the array
